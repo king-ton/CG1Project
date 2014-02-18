5674bffb7893c9985c8b235330f789f121ddc00b
king-ton <tfreitag93@gmail.com>
2014-02-18 14:35:03 +0100

    Fragment-Programm eingefügt

--- CGContext_FragmentOperations.cpp
+++ CGContext_FragmentOperations.cpp
@@ -64,12 +64,101 @@ bool CGContext::m_cgFragmentClipping(CGFragmentData& fragment)
 }
 
 //---------------------------------------------------------------------------
+// Anwenden der Oberflächen-, Material- und Beleuchtungseigenschaften auf
+// Fragment
+//
+// Übung 10      - Aufgabe 2   |  Funktion implementiert
+// Übung 11      - Aufgabe 2e  |  Textur-Eigenschaften werden berücksichtigt
+// Hausaufgabe 4 - Aufgabe 2   |  Verschiedene Lichtquellenobjekte sind
+//							   |  jetzt möglich
+//---------------------------------------------------------------------------
 void CGContext::m_cgFragmentProgram(CGFragmentData& fragment)
 {
-	// Run programmable fragment processing.
-	CGVec4 color;
-	m_fragmentProgram(fragment, color, m_uniforms);
-	fragment.varyings[CG_COLOR_VARYING] = color;
+	const CGFragmentData& in = fragment;
+	const CGUniformData& uniforms = m_uniforms;
+
+	CGVec4 clr = in.varyings[CG_COLOR_VARYING];
+	CGVec4 nrm = in.varyings[CG_NORMAL_VARYING];
+	CGVec4 txc = in.varyings[CG_TEXCOORD_VARYING];
+	CGVec4 pos = in.varyings[CG_POSITION_EYESPACE_VARYING];
+
+	// renormalize the normal
+	nrm = CGMath::normalize(nrm);
+
+	// Compute Blinn-Phong reflection model.
+	CGVec4 emis, ambiScene, ambi, diff, spec;
+	emis.set(0.0f, 0.0f, 0.0f, 0.0f); ambi.set(0.0f, 0.0f, 0.0f, 0.0f);
+	diff.set(0.0f, 0.0f, 0.0f, 0.0f); spec.set(0.0f, 0.0f, 0.0f, 0.0f);
+	ambiScene.set(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// emission
+	emis = uniforms.materialEmission;
+	// ambient of the scene
+	ambiScene = CGMath::mul(uniforms.materialAmbient, uniforms.sceneAmbient);
+	// ambient
+	ambi = CGMath::mul(uniforms.materialAmbient, uniforms.light0Ambient);
+
+	// L is vector direction from current point (pos) to the light source (m_uniforms.light0Position)
+	CGVec4 L;
+	if (uniforms.light0Position[W] == 0)
+		L = CGMath::normalize(uniforms.light0Position);
+	else
+		L = CGMath::normalize(CGMath::sub(uniforms.light0Position, pos));
+
+	// calculate dot product of nrm and L
+	float NdotL = CGMath::dot(nrm, L);
+
+	if (NdotL>0)
+	{
+		// diffuse
+		diff = CGMath::scale(CGMath::mul(uniforms.materialDiffuse, uniforms.light0Diffuse), NdotL);
+
+		// E is direction from current point (pos) to eye position
+		CGVec4 ePos; ePos.set(0.0f, 0.0f, 0.0f, 1.0f);
+		CGVec4 E = CGMath::normalize(CGMath::sub(ePos, pos));
+		// H is half vector between L and E
+		CGVec4 H = CGMath::normalize(CGMath::add(L, E));
+
+		// specular
+		float NdotH = CGMath::dot(nrm, H);
+
+		if (NdotH > 0.0F)
+			spec = CGMath::scale(CGMath::mul(uniforms.materialSpecular, uniforms.light0Specular), pow(NdotH, uniforms.materialShininess));
+	}
+
+	// sum up the final output color
+	float att, spotLight;
+	if (uniforms.light0Position[W] != 1)
+		att = 1.0F;
+	else
+		att = 1.0F / (
+		uniforms.light0ConstantAttenuation +
+		uniforms.light0LinearAttenuation * CGMath::length(CGMath::sub(uniforms.light0Position, pos)) +
+		uniforms.light0QuadraticAttenuation * pow(CGMath::length(CGMath::sub(uniforms.light0Position, pos)), 2));
+
+	if (uniforms.light0SpotCutoff == cos(180.0F))
+		spotLight = 1;
+	else {
+		CGVec4 S = CGMath::normalize(uniforms.light0SpotDirection);
+		float SdotL = CGMath::dot(S, CGMath::scale(L, -1));
+		SdotL = SdotL > 0 ? SdotL : 0;
+		if (SdotL < uniforms.light0SpotCutoff)
+			spotLight = 0.0F;
+		else
+			spotLight = pow(SdotL, uniforms.light0SpotExponent);
+	}
+
+	clr = CGMath::add(CGMath::add(CGMath::scale(CGMath::add(CGMath::add(spec, diff), ambi), (att*spotLight)), ambiScene), emis);
+
+	// Berücksichtigung der Textur-eigenschaften
+	if (uniforms.sampler.texture != NULL)
+		clr = CGMath::mul(clr, uniforms.sampler.texture->sample(txc));
+
+	// Explicitly set alpha of the color
+	clr[A] = uniforms.materialDiffuse[A];
+	// clamp color values to range [0,1]
+	clr = CGMath::clamp(clr, 0, 1);
+	fragment.varyings[CG_COLOR_VARYING] = clr;
 }
 
 //---------------------------------------------------------------------------
